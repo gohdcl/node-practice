4f89eefc631d848a66bbc5b2bb7a289f91c8bd18
Daniel Goh <gohdcl@gmail.com>
2015-05-12 12:34:36 -0700

    added a few routes

--- blog/controllers/articles.js
+++ blog/controllers/articles.js
@@ -1,20 +1,23 @@
+var posts = []; // Not persistent, gets overwritten each server restart
+
 module.exports.create = function (req, res) {
+  req.body.comments = [];
+  posts.push(req.body);
+  res.redirect("/articles");
+};
+
+module.exports.index = function(req, res) {
+  res.json(posts);
 };
 
 // rendering an HTML form to let users create a new post
 module.exports.new = function (req, res) {
-  res.format({"text/html": function() {
-  res.send("<!DOCTYPE html>" +
-    "<html>" +
-    "<head>" +
-    "<meta charset='UTF-8'>" +
-    "</head>" +
-    "<body>" +
-    "<form method='post' action='/articles'>" +
-    "<input-type='text' placeholder='name'>" +
-    "</form>" +
-    "</body>" +
-    "</html>");
+  var fs = require("fs");
+  // NOTE: fs.readFile path is from the current working directory, that of server.js
+  fs.readFile("./views/new-article.html", "utf8", function(err, data) {
+    if(err) {
+      throw err;
     }
+    res.send(data);
   });
 };
--- blog/server.js
+++ blog/server.js
@@ -1,6 +1,11 @@
 var express = require("express"),
     articles = require("./controllers/articles"),
+    bodyParser = require("body-parser"),
+    fs = require("fs"),
     app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ "extended":true }));
 
 // CRUD = Create, Read, Update, Delete
 // Think about what routes will do what
@@ -25,13 +30,12 @@ var notImplemented = function(req, res) {
       "text": ""
     }
 */
-var posts = [];
 
 // Blog articles
-app.get("/articles", notImplemented); // READ all blog articles
+app.get("/articles", articles.index); // READ all blog articles
 app.get("/articles/new", articles.new); // CREATE a new article via admin page
 app.get("/articles/:articleID", notImplemented); // READ a specific blog article
-app.post("/articles", notImplemented); // CREATE a new blog article
+app.post("/articles", articles.create); // CREATE a new blog article
 app.put("/articles/:articleID", notImplemented); // UPDATE a blog article
 app.delete("/articles/:articleID", notImplemented); // DELETE a blog article
 
